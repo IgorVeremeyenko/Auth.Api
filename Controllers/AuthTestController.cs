da1c212885c99a14ec1455bc64b68e485cf98efd
IgorVeremeyenko <72911973+IgorVeremeyenko@users.noreply.github.com>
2021-10-21 13:48:02 +0300

    Update AuthTestController.cs

--- Controllers/AuthTestController.cs
+++ Controllers/AuthTestController.cs
@@ -1,17 +1,30 @@
-﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Auth.Api.Models;
+
 
 namespace Auth.Api.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    [Authorize]
     public class AuthTestController : ControllerBase
     {
         [HttpGet]
         public IActionResult Get()
         {
-            return Ok();
+            var userIdentity = HttpContext.User.Identity;
+            if (userIdentity.IsAuthenticated)
+            {
+                return Ok(userIdentity.Name);
+            }
+            return Unauthorized();
         }
+
+        // [HttpGet("extended")]
+        // [RequirePermission("AccessExtended")]
+        // public IActionResult GetExtended()
+        // {
+        //     return Ok();
+        // }
     }
 }
