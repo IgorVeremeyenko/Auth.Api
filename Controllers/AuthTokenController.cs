0c2c6d6deb9bcdf800f99cd89c6ab907162175bb
IgorVeremeyenko <72911973+IgorVeremeyenko@users.noreply.github.com>
2021-10-21 13:47:39 +0300

    Update AuthTokenController.cs

--- Controllers/AuthTokenController.cs
+++ Controllers/AuthTokenController.cs
@@ -1,8 +1,7 @@
-﻿using Auth.Api.Models;
+using Auth.Api.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Mvc;
-using System.Security.Claims;
-using System.Collections.Generic;
-
+using Microsoft.Extensions.Configuration;
 
 namespace Auth.Api.Controllers
 {
@@ -10,6 +9,29 @@ namespace Auth.Api.Controllers
     [ApiController]
     public class AuthTokenController : ControllerBase
     {
-        
+        private readonly IAuthService authService;
+        private readonly IConfiguration configuration;
+        private const string authScheme = JwtBearerDefaults.AuthenticationScheme;
+
+        public AuthTokenController(IAuthService authService, IConfiguration configuration)
+        {
+            this.authService = authService;
+            this.configuration = configuration;
+        }
+
+        [HttpPost]
+        public IActionResult Login(LoginModel model)
+        {
+            var user = authService.Authenticate(model);
+            if (user is null)
+            {
+                return Unauthorized();
+            }
+
+            var claims = authService.CreateClaims(user);
+            string encodedJwt = authService.CreateJwt(claims, configuration["SigningKey"]);
+
+            return Ok(new { Token = encodedJwt });
+        }
     }
 }
