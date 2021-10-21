8b673924266f08f0f2baf45b99cc5e90a59d338d
IgorVeremeyenko <72911973+IgorVeremeyenko@users.noreply.github.com>
2021-10-21 13:48:16 +0300

    Update AuthCoockieController.cs

--- Controllers/AuthCoockieController.cs
+++ Controllers/AuthCoockieController.cs
@@ -1,45 +1,40 @@
-﻿using Auth.Api.Models;
+using Auth.Api.Models;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using System.Collections.Generic;
-
 using System.Threading.Tasks;
 
 namespace Auth.Api.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class AuthCoockieController : ControllerBase
+    public class AuthCookieController : ControllerBase
     {
-
         private readonly IAuthService authService;
 
-        public AuthCoockieController(IAuthService authService)
+        public AuthCookieController(IAuthService authService)
         {
             this.authService = authService;
         }
 
         [HttpPost]
-        public async Task<IActionResult>Login(LoginModel model)
+        public async Task<IActionResult> LoginAsync(LoginModel model)
         {
-            var user = authService.IsAuth(model);
+            var user = authService.Authenticate(model);
             if (user is null)
             {
                 return Unauthorized();
             }
 
+            var principal = authService.CreatePrincipal(user,
+                CookieAuthenticationDefaults.AuthenticationScheme);
 
-            var principal = authService.CreatePrincipal(user, CookieAuthenticationDefaults.AuthenticationScheme);
-
-            await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme,
+            await HttpContext.SignInAsync(
+                CookieAuthenticationDefaults.AuthenticationScheme,
                 principal,
                 null);
+
             return Ok();
         }
-
-       
-        
     }
 }
